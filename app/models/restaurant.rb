5822eeb499b7e01f589522fabe0cc3394d40e0cc
Derek Kaknes <derekkaknes@Dereks-MacBook-Pro.local>
2014-04-02 13:29:43 -0400

    Added Yelp API to get restaurants using Yelpster gem

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,17 +2,32 @@ class Restaurant < ActiveRecord::Base
    include Yelp::V1::Review::Request
    has_many :suggestions
 
-   client = Yelp::Client.new
 
    def get_restaurants(city,state, term='dinner')
-      request = Location.new(
-                  :city => city,
-                  :state => state,
-                  :radius => 2,
-                  :term => term,
-                  :limit => 5
-      )
-      response = client.search(request)
+     yelp_profiles = yelp_request(city, state, term='dinner')
+     restaurants = parse_yelp_profiles(yelp_profiles)
+     restaurants.sample
    end
+  
+  def yelp_request(city,state,term='dinner')
+    client = Yelp::Client.new
+     request = Location.new(
+       :compress_response => false,
+                 :city => city,
+                 :state => state,
+                 :radius => 2,
+                 :term => term,
+                 :limit => 5
+     )
+     response = client.search(request)
+  end
 
+  def parse_yelp_profiles(yelp_profiles)
+    businesses = yelp_profiles["businesses"]
+    business_names = []
+    businesses.each do |business|
+      business_names << business["name"]
+    end
+    business_names
+  end
 end
