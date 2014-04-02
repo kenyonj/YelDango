586e1470eb931a3e10d57c490d1c0c491f3aacc6
Justin Kenyon <kenyonj@gmail.com>
2014-04-02 00:47:22 -0400

    Update controller logic

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,9 +1,9 @@
 class SuggestionsController < ApplicationController
 
   def new
-    @city = session[:city]
-    @state = session[:state]
-    Event.get_events(@city, @state)
+    city = session[:city]
+    state = session[:state]
+    Event.get_events(city, state)
     # Restaurant.get_restaurants(@city, @state)
     @suggestion = Suggestion.new_suggestion
     render :show
