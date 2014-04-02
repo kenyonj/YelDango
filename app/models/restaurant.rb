bfaba6042e1f0f6c9b03b2480dbc4eca558e15c5
Derek Kaknes <derekkaknes@Dereks-MacBook-Pro.local>
2014-04-01 20:52:07 -0400

    Added Yelp API

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,18 @@
 class Restaurant < ActiveRecord::Base
+   include Yelp::V1::Review::Request
+   has_many :suggestions
+
+   client = Yelp::Client.new
+
+   def get_restaurants(city,state, term='dinner')
+      request = Location.new(
+                  :city => city,
+                  :state => state,
+                  :radius => 2,
+                  :term => term,
+                  :limit => 5
+      )
+      response = client.search(request)
+   end
+
 end
